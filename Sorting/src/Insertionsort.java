adbced7f2922254aa65871d388c07e11ea26a97b
Pratik rai <raipratik788@gmail.com>
2026-02-22 23:29:28 +0530

    Insertionsort

--- Sorting/src/Insertionsort.java
+++ Sorting/src/Insertionsort.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Insertionsort {
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=1; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+                else break;
+            }
+        }
+    }
 }
